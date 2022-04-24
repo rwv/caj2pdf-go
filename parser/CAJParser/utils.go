e7bc7f03a48a86c315e0cef21026c28aa36902de
Seedgou <i@zczc.cz>
2022-04-24 14:08:48 +0000

    add findReverse

--- parser/CAJParser/utils.go
+++ parser/CAJParser/utils.go
@@ -54,3 +54,67 @@ func find(file io.ReadSeeker, pattern []byte, start int64) int64 {
 		}
 	}
 }
+
+func findReverse(file io.ReadSeeker, pattern []byte, end int64) int64 {
+	patternLen := len(pattern)
+	fsize, _ := file.Seek(0, io.SeekEnd)
+	const bsize int64 = 4096
+	if int64(patternLen) > end {
+		panic("Too large string size for search.")
+	}
+
+	file.Seek(fsize-bsize, io.SeekStart)
+
+	size := bsize
+	if bsize <= end && end < fsize {
+		file.Seek(end-bsize, io.SeekStart)
+	} else if 0 < end && end < bsize {
+		size = end
+		file.Seek(0, io.SeekStart)
+	}
+
+	overlap := int64(patternLen - 1)
+
+	patternReverse := make([]byte, len(pattern))
+
+	for i := 0; i < len(pattern); i++ {
+		patternReverse[i] = pattern[len(pattern)-1-i]
+	}
+
+	for {
+		buf := make([]byte, size)
+		n, _ := file.Read(buf)
+		currentOffset, _ := file.Seek(0, io.SeekCurrent)
+		if n > 0 {
+			bufReverse := make([]byte, n)
+			for i := 0; i < n; i++ {
+				bufReverse[i] = buf[n-1-i]
+			}
+
+			// Find Pos
+			var pos int64 = -1
+
+			for i := 0; i < n-patternLen; i++ {
+				if bytes.Compare(bufReverse[i:i+patternLen], patternReverse) == 0 {
+					pos = int64(i)
+					break
+				}
+			}
+
+			if pos >= 0 {
+				return currentOffset - pos
+			}
+		}
+
+		if (2*bsize - overlap) < currentOffset {
+			file.Seek(currentOffset-(2*bsize-overlap), io.SeekStart)
+			size = bsize
+		} else if (bsize - overlap) < currentOffset {
+			size = currentOffset - (bsize - overlap)
+			file.Seek(0, io.SeekStart)
+		} else {
+			return -1
+		}
+	}
+
+}

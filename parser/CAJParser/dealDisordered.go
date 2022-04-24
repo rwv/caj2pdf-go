fbfff8073c085f6b2ec63fc387d218a9923ff447
Seedgou <i@zczc.cz>
2022-04-24 14:21:01 +0000

    add dealDisordered

--- parser/CAJParser/dealDisordered.go
+++ parser/CAJParser/dealDisordered.go
@@ -1,7 +1,45 @@
 package CAJParser
 
-import "io"
+import (
+	"io"
+	"strconv"
+)
 
 func dealDisordered(reader io.ReadSeeker) {
+	endobjSlice := findAllOccurances(reader, []byte("endobj"))
+	obj_no := []int64{}
 
+	for _, endobjAddress := range endobjSlice {
+		startObj := findReverse(reader, []byte(" 0 obj"), endobjAddress)
+		startObj1 := findReverse(reader, []byte("\r"), startObj)
+		startObj2 := findReverse(reader, []byte("\n"), startObj)
+		// startObj = Max(startObj1, startObj2)
+		if startObj1 > startObj2 {
+			startObj = startObj1
+		} else {
+			startObj = startObj2
+		}
+		length := find(reader, []byte(" "), startObj) - startObj
+
+		reader.Seek(startObj, io.SeekStart)
+		var numberStringBytes []byte = make([]byte, length)
+		reader.Read(numberStringBytes)
+		number_, _ := strconv.Atoi(string(numberStringBytes))
+		number := int64(number_)
+		if !contains(obj_no, number) {
+			obj_no = append(obj_no, number)
+			// obj_len = addr - startobj + 6 // from original caj2pdf
+			reader.Seek(startObj, io.SeekStart)
+			// [obj] = struct.unpack(str(obj_len) + "s", pdf.read(obj_len)) // from original caj2pdf
+		}
+	}
+}
+
+func contains(s []int64, e int64) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }

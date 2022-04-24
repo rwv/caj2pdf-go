829a223af9ef7d20da7b3c01e54d0b7c52a1fc96
Seedgou <i@zczc.cz>
2022-04-24 11:27:00 +0000

    PDF Parser Convert

--- parser/PDFParser.go
+++ parser/PDFParser.go
@@ -1,5 +1,10 @@
 package parser
 
+import (
+	"io"
+	"os"
+)
+
 type PDFParser struct {
 	filePath string
 }
@@ -8,4 +13,20 @@ func NewPDFParser(filePath string) PDFParser {
 	return PDFParser{
 		filePath: filePath,
 	}
+}
+
+func (parser PDFParser) Convert(target string) error {
+	// Just change the file extension to .pdf is enough
+	source, err := os.Open(parser.filePath)
+	if err != nil {
+		return err
+	}
+
+	destination, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(destination, source)
+	return err
 }
